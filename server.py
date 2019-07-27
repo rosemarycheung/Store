8236204ab6e2c7adb2c6341a14866326317d4a63
rosemarycheung <rosemaryrosec@gmail.com>
2019-07-26 17:18:48 -0700

    Resume

---
+++ server.py
@@ -0,0 +1,10 @@
+from flask import Flask, escape, request, render_template
+
+app = Flask(__name__, static_url_path='')
+
+@app.route('/')
+def home_page():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run()
